5c2d0f88fa38967a21ee1f5d6bede9469e0c50ab
cnrgrl <cnrgrl@gmail.com>
2024-08-28 19:55:01 +0200

    ec2 and s3 sources created

---
+++ terraform-variables-conditionals-loops/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  #access_key = "my-access-key"
+  #secret_key = "my-secret-key"
+}
+
+# Create an ec2 instance (aws linux)
+resource "aws_instance" "tf-ec2" {
+#   ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-066784287e358dad1"
+  instance_type = "t2.micro"
+  key_name = "cnr-key-1"
+
+  tags = {
+    Name = "from-local-windows"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "cnr-tf-test-bucket-1"
+}
