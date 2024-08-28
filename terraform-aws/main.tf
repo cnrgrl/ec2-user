7b22758f677a95fb2e5b9502162f7085c43979a8
cnrgrl <ugrl.cnr@gmail.com>
2024-08-28 00:00:16 +0000

    'terraform fmt' formatted the .tf file

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.55.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
   # access_key = "my-access-key"
   # secret_key = "my-secret-key"
   ## profile = "my-profile"
@@ -17,6 +17,7 @@ provider "aws" {
 resource "aws_instance" "tf-ec2" {
   ami           = "ami-01b799c439fd5516a"
   instance_type = "t2.micro"
+
   key_name = "cnr-key-1"
   tags = {
     "Name" = "created-by-tf"
