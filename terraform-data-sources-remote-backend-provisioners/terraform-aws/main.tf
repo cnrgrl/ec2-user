3232f057b60eb54fd54e7ec3dbc142b531e67d69
cnrgrl <cnrgrl@gmail.com>
2024-09-12 23:17:25 +0200

    ec2 instance from image

---
+++ terraform-data-sources-remote-backend-provisioners/terraform-aws/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+}
+
+locals {
+  mytag = "cnr-local-name"
+}
+
+data "aws_ami" "tf_ami" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = data.aws_ami.tf_ami.id
+  instance_type = var.ec2_type
+  key_name      = "cnr-key-1"
+  tags = {
+    Name = "${local.mytag}-this is from my-ami"
+  }
+}
