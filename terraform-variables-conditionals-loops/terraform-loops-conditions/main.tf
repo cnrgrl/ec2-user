5c65401cdc8ac3a6b3bc31e446bcbe93f40f7bee
cnrgrl <cnrgrl@gmail.com>
2024-09-12 18:41:21 +0200

    two s3 bucket created with count expression

---
+++ terraform-variables-conditionals-loops/terraform-loops-conditions/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-east-1"
+  }
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version ="5.30.0"
+    }
+  }
+}
+
+variable "num_of_buckets" {
+    default = 2
+}
+
+variable "s3_bucket_name" {
+  default = "counted-buckets-cnr"
+}
+
+resource "aws_s3_bucket" "tf-s3-cnr" {
+  bucket = "${var.s3_bucket_name}-${count.index}"
+  count = var.num_of_buckets
+}
