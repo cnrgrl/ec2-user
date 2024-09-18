bc375daefd87c7c63e32aeacde72a0929c75a483
cnrgrl <cnrgrl@gmail.com>
2024-09-19 00:47:02 +0200

    existing securty group imported

--- terraform-modules_Import/terraform-import/main.tf
+++ terraform-modules_Import/terraform-import/main.tf
@@ -1,10 +1,4 @@
 import {
-  to = aws_instance.example
-  id = "i-099cf28d20079515f"
-}
-
-resource "aws_instance" "example" {
-  ami = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
-  # (resource arguments...)
+  to = aws_security_group.mysg
+  id = "sg-0d35e36a05c8e9c6b"
 }
