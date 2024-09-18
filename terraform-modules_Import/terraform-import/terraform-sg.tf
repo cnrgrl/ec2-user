ad323d2c4290e08cf92c16589562c88e21ae522f
cnrgrl <cnrgrl@gmail.com>
2024-09-19 00:48:25 +0200

    sg tf config file created via "terraform plan -generate-config-out=terraform-sg.tf"

---
+++ terraform-modules_Import/terraform-import/terraform-sg.tf
@@ -0,0 +1,25 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "sg-0d35e36a05c8e9c6b"
+resource "aws_security_group" "mysg" {
+  description = "terraform-sg"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress                = []
+  name                   = "terraform-sg"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-0755c775b98d44b75"
+}
