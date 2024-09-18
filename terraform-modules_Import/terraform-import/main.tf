2cf4d01ea8e0ab4b6dbdc9e0f80a646bb5f51aed
cnrgrl <cnrgrl@gmail.com>
2024-09-19 00:37:34 +0200

    resource imported via "import{....}" block in main.tf

--- terraform-modules_Import/terraform-import/main.tf
+++ terraform-modules_Import/terraform-import/main.tf
@@ -1,21 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.49.0"
-    }
-  }
+import {
+  to = aws_instance.example
+  id = "i-099cf28d20079515f"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "tf-instances" {
-  ami = "ami-0e86e20dae9224db8"
+resource "aws_instance" "example" {
+  ami = "ami-0ebfd941bbafe70c6"
   instance_type = "t2.micro"
-  key_name = "cnr-key-1"
-  tags = {
-    Name = "ubuntu-24.04"
-  }
+  # (resource arguments...)
 }
